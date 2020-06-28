280e7b9631fb6588eddfadca0d6b02e56fb550c6
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-28 03:06:47 +0000

    index page structure

--- Rakefile
+++ Rakefile
@@ -1,2 +1,8 @@
 require "bundler/gem_tasks"
 task :default => :spec
+
+require_relative './config/environment'
+require 'sinatra/activerecord/rake'
+task :console do
+  Pry.start 
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
-class ApplicationController
-  
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+		set :session_secret, "password_security"
+  end
+
+  get '/' do
+    erb :index
+  end
+
 end 
